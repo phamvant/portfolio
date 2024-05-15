cb5ac745ce23d2373038e73e4a6a33e2b5cd2f61
phamvant <pham.t.286.01@gmail.com>
2024-05-15 23:50:18 +0900

    add tech stack

--- app/components/timeline.tsx
+++ app/components/timeline.tsx
@@ -1,18 +1,22 @@
 import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/all";
 import gsap from "gsap";
-import Image from "next/image";
 
 const list = [
-  { time: "Jun, 2019", event: "Hanoi University Of Science And Technology" },
+  { time: "Jun, 2019", company: "Hanoi University Of Science And Technology" },
   {
     time: "Jun, 2021",
-    event: "Sun Asterisk - PHP ・ Laravel ・ Backend Developer",
+    company: "Sun Asterisk",
+    stack: "PHP ・ Laravel ・ Backend Developer",
   },
-  { time: "Feb, 2022", event: "FPT Japan ・ C# ・ Unity ・ Mobile Developer" },
+  {
+    time: "Feb, 2022",
+    company: "FPT Japan",
+    stack: "C# ・ Unity ・ Mobile Developer",
+  },
   {
     time: "Feb, 2023",
-    event: "Morson Japan ・ C# ・ WPF ・ Flutter ・ Mobile Developer",
+    company: "Morson Japan",
+    stack: "C# ・ WPF ・ Flutter ・ Mobile Developer",
   },
 ];
 
@@ -53,14 +57,17 @@ export default function Timeline() {
                 after:border-slate-50 after:rounded-full  after:-translate-x-1/2 after:translate-y-1.5 "
                           ></div>
                           <time
-                            className="md:pl-4 w-fit sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs 
-                  font-semibold uppercase w-20 h-6 sm:mb-0 text-emerald-600  rounded-full"
+                            className="pl-4 w-fit sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs 
+                  font-semibold uppercase h-6 sm:mb-0 text-emerald-600  rounded-full"
                           >
                             {val.time}
                           </time>
                         </div>
+                        <div className="pl-6 mb-2 font-normal text-gray-100 max-w-60 md:max-w-fit text-md">
+                          {val.company}
+                        </div>
                         <div className="pl-6 mb-2 font-normal text-gray-100 text-md">
-                          {val.event}
+                          {val.stack}
                         </div>
                       </div>
                     );
