1e6ce5969f2e280aa7887994c2a6ef5dd5474fb6
thuan <pham.t.286.01@gmail.com>
2024-06-04 23:42:25 +0900

    update animation

--- app/components/project/project-card.tsx
+++ app/components/project/project-card.tsx
@@ -9,20 +9,22 @@ export interface ProjectInfo {
 
 export function ProjectCard({ project }: { project: ProjectInfo }) {
   return (
-    <div
-      className="w-full border-[1px] max-h-80 group relative flex flex-col dark:bg-slate-900/50
-        rounded-xl shadow-md hover:shadow-2xl overflow-hidden transition-all duration-300 h-full"
-    >
-      <img
-        src={project.imgUrl}
-        alt="Post Image"
-        className="object-cover w-auto min-h-40"
-      />
-      <div className="absolute inset-0 bg-black bg-opacity-0 dark:bg-opacity-20 group-hover:bg-opacity-0 transition-all duration-300"></div>
-      <div className="p-4 border-t-[1px] border-gray-300 flex h-full flex-1 justify-between">
-        <p className="h-fit">{project.describe}</p>
-        <div className="flex items-end">
-          <FaGithub size={20} />
+    <div className="flex justify-center">
+      <div
+        className="w-[95%] h-[95%] hover:w-full hover:h-full border-[1px] max-h-80 group relative flex flex-col dark:bg-slate-900/50
+        rounded-xl shadow-md hover:shadow-2xl overflow-hidden transition-all duration-300"
+      >
+        <img
+          src={project.imgUrl}
+          alt="Post Image"
+          className="object-cover w-auto min-h-40"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-0 dark:bg-opacity-20 group-hover:bg-opacity-0 transition-all duration-300"></div>
+        <div className="p-4 border-t-[1px] border-gray-300 flex h-full flex-1 justify-between">
+          <p className="h-fit">{project.describe}</p>
+          <div className="flex items-end">
+            <FaGithub size={20} />
+          </div>
         </div>
       </div>
     </div>
