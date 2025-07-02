750d8369d8a9190f1cbb4ab1a355c0056f8c9c88
phamvant <pham.t.286.01@gmail.com>
2025-07-02 17:40:22 +0900

    refactor

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -1,52 +1,74 @@
 import { ReactNode } from "react";
 import Markdown from "./markdown";
+import { DataProps } from "@/app/page";
 
-export default function Project({ children }: { children: ReactNode }) {
+
+export default function Project({ children, data }: { children: ReactNode, data: DataProps }) {
   return (
     <div>
-      <div className="project-1 py-32 md:py-52 w-full flex flex-wrap gap-40 lg:gap-10 *:min-w-[300px] *:flex-1 md:px-[5%] xl:px-[20%] bg-white">
+      <div className={`${data.settings.isLeft ? "flex flex-row-reverse lg:gap-32" : "flex lg:gap-10"} project-1 py-32 md:py-52 w-full flex-wrap gap-40 *:min-w-[300px] *:flex-1 md:px-[5%] xl:px-[10%] bg-white"`}>
         <div className="flex flex-col gap-10 px-4 md:px-0">
-          <p className="text-3xl text-blue-400">2023</p>
+          <p className="text-3xl text-blue-400">{data.year}</p>
           <p className="text-4xl">
-            ComputeHub
+            {data.nameEn}
             <br />
             <br />
-            分散コンピューティング
+            {data.nameJa}
           </p>
           <p
             className="text-xl w-full md:w-3/4 border-2 border-slate-300 p-4 
           rounded-2xl shadow-xl hover:shadow-none transition-shadow duration-300"
           >
-            ブラウザーでコンピューティングパワーを共有するプロジェクト。「PandoComputingコアモジュールを使用」
+            {data.short_des}
           </p>
           <Markdown>{children}</Markdown>
         </div>
-        <div className="relative group  *:absolute *:left-10 *:rounded-2xl *:shadow-2xl *:transition-all *:duration-300 pb-64">
-          <img
-            src="/project/dreamhacker/monitor_1.png"
-            className="w-3/4 lg:w-full lg:group-hover:scale-110 translate-x-10 lg:group-hover:-translate-x-[10%]"
-          />
-          <img
-            src="/project/js/computehub.png"
-            className="w-4/5 lg:w-full object-cover lg:group-hover:scale-50  lg:group-hover:translate-x-[75%] lg:group-hover:translate-y-4"
-          />
-        </div>
+        {data.settings.isMobile ?
+          <div className="relative group *:lg:scale-[60%]  *:absolute *:left-10 *:rounded-2xl *:shadow-2xl *:transition-all *:duration-300 pb-64">
+            <img
+              src={data.images[0]}
+              className="w-3/4 lg:w-full lg:group-hover:-translate-y-60 lg:group-hover:-translate-x-80"
+            />
+            <img
+              src={data.images[1]}
+              className="w-3/4 lg:w-full object-cover translate-x-5 lg:-translate-x-10 -translate-y-10 lg:group-hover:-translate-y-60 lg:group-hover:translate-x-40"
+            />
+            <img
+              src={data.images[2]}
+              className="w-3/4 lg:w-full object-cover translate-x-10 lg:-translate-x-20 -translate-y-20 lg:group-hover:-translate-y-64"
+            />
+          </div> :
+          <div className="relative group  *:absolute *:left-10 *:rounded-2xl *:shadow-2xl *:transition-all *:duration-300 pb-64">
+            <img
+              src={data.images[0]}
+              className="w-3/4 lg:w-full lg:group-hover:scale-110 translate-x-10 lg:group-hover:-translate-x-[10%]"
+            />
+            <img
+              src={data.images[1]}
+              className="w-4/5 lg:w-full object-cover lg:group-hover:scale-50  lg:group-hover:translate-x-[75%] lg:group-hover:translate-y-4"
+            />
+            {data.images[2] ? <img
+              src={data.images[2]}
+              className="w-4/5 lg:w-full object-cover lg:group-hover:scale-50  lg:group-hover:translate-x-[75%] lg:group-hover:translate-y-4"
+            /> : <></>}
+          </div>
+        }
       </div>
-      <div className="flex flex-col justify-center items-center bg-blue-100/60 relative">
+      <div className="flex flex-col justify-center items-center bg-blue-100 relative">
         <div className="flex flex-col justify-center items-center md:px-[5%] xl:px-[20%] pt-16 pb-16 w-3/4">
           <img
-            src="/compute.gif"
+            src={data.gif}
             className="object-cover rounded-xl shadow-2xl hover:shadow-none transition-shadow duration-300"
           />
         </div>
       </div>
       <div className="flex flex-wrap h-1/4 bg-white px-[5%] xl:px-[20%] py-20 justify-center text-lg gap-10">
         <p className="w-full lg:w-3/4">
-          提案された解決策は、個人がコンピュータ、ノートパソコン、モバイルフォンなどの個人デバイスを通じて計算能力を提供できるボランティアコンピューティングシステムを構築することです。このシステムは、ボランティアが登録し、計算能力を提供し、研究プロジェクトに参加できるように、シンプルで使いやすいユーザーインターフェースを提供します。同時に、高度なアルゴリズムを使用してタスクを管理し、リソースを分配し、計算パフォーマンスを最適化します。
+          {data.long_des}
         </p>
 
         <div className="flex items-center">
-          <a href="https://compute-hub-fe-vert.vercel.app/" target="_blank">
+          <a href={data.link} target="_blank">
             <button className="bg-blue-400 w-fit p-2 px-4 rounded-3xl hover:underline">
               ページを開く
             </button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,97 @@
 import Highlight from "./components/highlight";
 import Project from "./components/project/project";
-import Project2 from "./components/project/project-2";
-import Project3 from "./components/project/project-3";
 import ComputeHub from "@/public/content/ComputeHub.mdx";
 import Dreamhacker from "@/public/content/Dreamhacker.mdx";
 import Lychie from "@/public/content/Lychie.mdx";
+
+export interface PageSettings {
+  isMobile: boolean,
+  isLeft: boolean
+}
+
+export interface DataProps {
+  year: string,
+  nameEn: string,
+  nameJa: string,
+  short_des: string,
+  long_des: string,
+  images: string[],
+  gif: string,
+  link: string
+  settings: PageSettings
+  mdx: any
+}
+
+const data: Record<string, DataProps> = {
+  "dreamhacker": {
+    year: "2024",
+    nameEn: "Dreamhacker",
+    nameJa: "若い者向け留学体験共有Forum。",
+    short_des: "応募体験、勉強方法、大学評判等。",
+    long_des: "国際的なオンラインおよびオフラインのコミュニティを発展させることに専念しています。メンバーはGMATやTOEFLの準備、ビジネススクールへの出願、学校生活、キャリア開発、ビジネスの機会を探るなどの経験を共有することができます。他者が個人的および専門的な目標を達成する手助けをし、メンバー同士の信頼できる長期的な関係を築くことを目指しています。",
+    images: [
+      "/project/dreamhacker/4.png",
+      "/project/dreamhacker/5.png",
+      "/project/dreamhacker/6.png",
+    ],
+    gif: "/ecom.gif",
+    link: "https://dream-hacker.com",
+    settings: {
+      isMobile: false,
+      isLeft: false
+    },
+    mdx: <Dreamhacker />
+  },
+  "lychie": {
+    year: "2024",
+    nameEn: "Store Management",
+    nameJa: "ストア管理",
+    short_des: "私の店舗を管理するために、包括的なEコマース店舗管理システムを開発しました。",
+    long_des: "製品および在庫管理、注文処理、顧客関係管理、支払い処理を含む包括的なEコマースストア管理システムを開発しました。フロントエンドにはNext.js、バックエンドにはNest.js、インフラにはAWSを使用しています。ログインしてご自身で体験することができます。",
+    images: [
+      "/project/lychie/3.jpg",
+      "/project/lychie/2.jpg",
+      "/project/lychie/1.jpg",
+    ],
+    gif: "/ecom.gif",
+    link: "https://lychie.vercel.app",
+    settings: {
+      isMobile: true,
+      isLeft: true
+    },
+    mdx: <Lychie />
+  }, 
+
+  "compute_hub": {
+    year: "2023",
+    nameEn: "ComputeHub",
+    nameJa: "分散コンピューティング",
+    short_des: "ブラウザーでコンピューティングパワーを共有するプロジェクト。「PandoComputingコアモジュールを使用」",
+    long_des: "提案された解決策は、個人がコンピュータ、ノートパソコン、モバイルフォンなどの個人デバイスを通じて計算能力を提供できるボランティアコンピューティングシステムを構築することです。このシステムは、ボランティアが登録し、計算能力を提供し、研究プロジェクトに参加できるように、シンプルで使いやすいユーザーインターフェースを提供します。同時に、高度なアルゴリズムを使用してタスクを管理し、リソースを分配し、計算パフォーマンスを最適化します。",
+    images: [
+      "/project/computehub/2.png",
+      "/project/computehub/1.png",
+    ],
+    gif: "/compute.gif",
+    link: "https://compute-hub-fe-vert.vercel.app",
+    settings: {
+      isMobile: false,
+      isLeft: false
+    },
+    mdx: <ComputeHub />
+  },
+
+}
 
 export default async function Home() {
   return (
     <div>
       <Highlight />
-      <Project>
-        <ComputeHub />
-      </Project>
-      <Project2>
-        <Lychie />
-      </Project2>
-      <Project3>
-        <Dreamhacker />
-      </Project3>
+      {Object.values(data).map((project) =>
+        <Project data={project} key={project.link}>
+          {project.mdx}
+        </Project>
+      )}
     </div>
   );
 }
